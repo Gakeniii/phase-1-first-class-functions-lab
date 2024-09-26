5acdd5c58af5278e45afb5a0452f18c460ad0ad3
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-09-26 22:40:15 +0300

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+//             FIRST DRIVERS
+const theDrivers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnFirstTwoDrivers = function(){
+    return theDrivers.slice(0,2);
+    
+}
+returnFirstTwoDrivers();
+
+//           LAST DRIVERS
+const returnLastTwoDrivers = function(){
+    return theDrivers.slice(2,4);
+}
+returnLastTwoDrivers();
+
+//         SELECTING DRIVERS
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//           FARE MULTIPLER
+
+const createFareMultiplier = (num1) => (integer) =>{
+    return num1 * integer;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+//     SELECTING DRIVERS
+
+const selectDifferentDrivers = function(theDrivers, selectDrivers){
+    return selectDrivers(theDrivers)
+};
